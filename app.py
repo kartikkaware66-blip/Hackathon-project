f4a5e2d7f7a247b74a8460b1156976c479c3e7ef
Prasad Wankhade <prasadw350@gmail.com>
2026-08-14 19:58:12 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,4 @@
+print("Hello Guys!!!")
+
+print("""Step 1: git clone https://github.com/kartikkaware66-blip/Hackathon-project.git (copy-paste-run kra)
+Step 2: folder select kra aani next je Terminal madhi code takle te (git codes below for github commit)""")
